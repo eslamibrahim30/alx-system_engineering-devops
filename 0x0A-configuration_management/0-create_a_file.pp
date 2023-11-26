07af066b94524efee5aa53e15db9ee1f74fd4825
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-26 02:18:20 +0200

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # create a file in /tmp
-file { 'school':
-  ensure  => present,
-  path    => '/tmp',
+file { '/tmp/school':
+  ensure  => 'file',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
