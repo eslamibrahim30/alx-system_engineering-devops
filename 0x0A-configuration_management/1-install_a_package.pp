bbbf4556eccce4e7877f3f28a1a2b913db9074fe
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-26 15:56:09 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,4 @@
 # install flask 2.1.0 from pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',
-  require  => Package['python3-pip'],
-  provider => 'pip',
 }
