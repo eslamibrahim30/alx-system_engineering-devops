d11819ad6cafedb8247c91f0565ef78da064d25b
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-26 17:42:05 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
 # install flask 2.1.0 from pip3
-file { '/opt/graphite/bin/flask-cache.py':
-  ensure => 'absent',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
-  require  => [ Class['graphite::prereqs::install'],
-              File['/opt/graphite/bin/flask-cache.py']
-              ]
   provider => pip,
 }
