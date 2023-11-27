465ca33043834e253ddf393fbfff5bc85543d8d6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-27 02:19:58 +0200

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,28 @@
 # set up your client SSH configuration file
-file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+file { '/home/$user/.ssh/':
+  ensure => directory,
 }
 
-file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+file { '/home/$user/.ssh/school':
+  ensure => file,
+}
+
+file { '/etc':
+  ensure => directory,
+}
+
+file { '/etc/ssh':
+  ensure => directory,
+}
+
+file { '/etc/ssh/ssh_config':
+  ensure => file,
+}
+
+exec { 'echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/ssh_config > /dev/null':
+  path => ['/usr/bin', '/usr/sbin',],
+}
+
+exec { 'echo "IdentityFile ~/.ssh/school" | sudo tee -a /etc/ssh/ssh_config > /dev/null':
+  path => ['/usr/bin', '/usr/sbin',],
 }
