25c50d359ebf47566fc2d07fceffdd27b8c0beb4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-11-27 02:21:49 +0200

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,24 +1,4 @@
 # set up your client SSH configuration file
-file { '/home/$user/.ssh/':
-  ensure => directory,
-}
-
-file { '/home/$user/.ssh/school':
-  ensure => file,
-}
-
-file { '/etc':
-  ensure => directory,
-}
-
-file { '/etc/ssh':
-  ensure => directory,
-}
-
-file { '/etc/ssh/ssh_config':
-  ensure => file,
-}
-
 exec { 'echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/ssh_config > /dev/null':
   path => ['/usr/bin', '/usr/sbin',],
 }
