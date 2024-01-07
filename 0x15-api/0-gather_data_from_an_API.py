dbc3d50a00c7775b9e84d2a12bb7a4e2a9c382d6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-07 09:40:24 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,22 +6,20 @@ import sys
 
 if __name__ == "__main__":
     employeeID = int(sys.argv[1])
-    users = requests.get('https://jsonplaceholder.typicode.com/users/').json()
-    for user in users:
-        if user.get("id") == employeeID:
-            EMPLOYEE_NAME = user.get("name")
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(employeeID)).json()
+    EMPLOYEE_NAME = user.get("name")
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
-    UNDONE_TASKS = []
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
+    DONE_TASKS = []
+    todos = requests.get(url + 'todos/').json()
     for task in todos:
         if task.get("userId") == employeeID:
             TOTAL_NUMBER_OF_TASKS += 1
             if task.get("completed") is True:
                 NUMBER_OF_DONE_TASKS += 1
-            else:
-                UNDONE_TASKS.append(task.get("title"))
+                DONE_TASKS.append(task.get("title"))
     print("Employee {} is done with tasks".format(EMPLOYEE_NAME), end="")
     print("({}/{}):".format(NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in UNDONE_TASKS:
+    for task in DONE_TASKS:
         print("\t {}".format(task))
