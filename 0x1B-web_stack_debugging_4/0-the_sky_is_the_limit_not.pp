43e3407b18f08170d4e7572b506cb42477e10163
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-01-25 11:57:54 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,4 @@
 # This manifest to increase the soft limit
-limits::limits{ 'build_nproc':
-  ensure     => present,
-  user       => 'build',
-  limit_type => 'nproc',
-  hard       => 512,
-  soft       => 256,
+exec {"fix--for-nginx":
+        command => "/bin/bash -c 'ulimit -c unlimited'",
 }
